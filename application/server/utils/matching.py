5aee1c01cba221a148bc35e8826fb8dfe91bb340
Hachiman <141888124+Hachiman-potassiumdesu@users.noreply.github.com>
2025-11-16 12:49:47 -0800

    hehe

--- application/server/utils/matching.py
+++ application/server/utils/matching.py
@@ -18,23 +18,6 @@ def l2_distance(a, b):
 std = 0.75
 
 def similarities(expectation, actual):
-<<<<<<< Updated upstream
-    sim = []
-    for id, a in enumerate(actual):
-        sim.append([cosine_similarity(expectation, a), id])
-
-    return sorted(sim, key=lambda x: x[0])
-
-expectation = np.array([[10, 2], [3, 3], [4, 1], [5, 0]])
-actual = [
-    np.array([1, 2, 3, 4]),
-    np.array([2, 3, 4 ,5]),
-    np.array([3, 4, 5, 6]),
-    np.array([10, 3, 4, 5])
-]
-
-print(similarities(expectation, actual))
-=======
     distances = []
     for a in actual:
         distances.append(l2_distance(a, expectation))
@@ -44,9 +27,4 @@ print(similarities(expectation, actual))
     distances_normalized = np.exp((-distances ** 2) / (2 * std ** 2))
 
     return distances_normalized, distances
-    # sim = []
-    # for a in actual:
-    #     sim.append(cosine_similarity(expectation, a))
 
-    # return sim
->>>>>>> Stashed changes
